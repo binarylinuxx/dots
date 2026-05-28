f1f9e89c7d31d73e5bbe035e830abdf4ddc460b8
binarylinuxx <aar58384@gmail.com>
2026-05-28 21:43:56 +0300

    fix

--- nix-deps/modules/nixos.nix
+++ nix-deps/modules/nixos.nix
@@ -32,45 +32,46 @@ in {
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    services.greetd = {
-      enable = true;
-      settings.default_session = {
-        user = cfg.user;
-        command = lib.concatStringsSep " " ([
-          "${cfg.package}/bin/greeter-launcher"
-          "--cache-dir"
-          (toString cfg.cacheDir)
-        ] ++ lib.optional cfg.debug "--debug");
-      };
-    };
-
-    users.users.${cfg.user} = {
-      isSystemUser = true;
-      group = cfg.user;
-      home = toString cfg.cacheDir;
-      createHome = false;
-    };
-
-    users.groups.${cfg.user} = { };
-
-    systemd.tmpfiles.rules = [
-      "d ${toString cfg.cacheDir} 0750 ${cfg.user} ${cfg.user} - -"
-    ];
-
-    environment.systemPackages = [ cfg.package ];
-    security.polkit.enable = lib.mkDefault true;
-  };
-
   options.services.blxshellMprisFixForZen = {
     enable = lib.mkEnableOption "blxshell MPRIS fix for Zen Browser";
   };
 
-  config = lib.mkIf mprisCfg.enable {
-    environment.systemPackages = [ pkgs.kdePackages.plasma-browser-integration ];
-    services.playerctld.enable = true;
-    environment.sessionVariables = {
-      MOZ_APP_SYSTEM_DIRS = "/run/current-system/sw/lib/mozilla";
-    };
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      services.greetd = {
+        enable = true;
+        settings.default_session = {
+          user = cfg.user;
+          command = lib.concatStringsSep " " ([
+            "${cfg.package}/bin/greeter-launcher"
+            "--cache-dir"
+            (toString cfg.cacheDir)
+          ] ++ lib.optional cfg.debug "--debug");
+        };
+      };
+
+      users.users.${cfg.user} = {
+        isSystemUser = true;
+        group = cfg.user;
+        home = toString cfg.cacheDir;
+        createHome = false;
+      };
+
+      users.groups.${cfg.user} = { };
+
+      systemd.tmpfiles.rules = [
+        "d ${toString cfg.cacheDir} 0750 ${cfg.user} ${cfg.user} - -"
+      ];
+
+      environment.systemPackages = [ cfg.package ];
+      security.polkit.enable = lib.mkDefault true;
+    })
+    (lib.mkIf mprisCfg.enable {
+      environment.systemPackages = [ pkgs.kdePackages.plasma-browser-integration ];
+      services.playerctld.enable = true;
+      environment.sessionVariables = {
+        MOZ_APP_SYSTEM_DIRS = "/run/current-system/sw/lib/mozilla";
+      };
+    })
+  ];
 }
